9b92a5913b529401f39a262f5ae038a389f5739a
Philip Mateescu <github@philipm.at>
2019-04-06 12:14:11 -0500

    removed unused namespaces

--- NameCheap/Domain/DomainCheckResult.cs
+++ NameCheap/Domain/DomainCheckResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
-
-namespace NameCheap
+﻿namespace NameCheap
 {
     public class DomainCheckResult
     {
--- NameCheap/Domain/DomainCreateRequest.cs
+++ NameCheap/Domain/DomainCreateRequest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NameCheap
+﻿namespace NameCheap
 {
     public class DomainCreateRequest
     {
