a1c07ca5a754223422115b7ffc4686c604ede67e
Philip Mateescu <github@philipm.at>
2019-04-07 00:52:38 -0500

    documentation of main methods

--- NameCheap/Dns/DnsApi.cs
+++ NameCheap/Dns/DnsApi.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace NameCheap
 {
+    /// <summary>Set of functions used to manipulate DNS records: hosts, name servers, and even email forwarding.</summary>
     public class DnsApi
     {
         private readonly XNamespace _ns = XNamespace.Get("http://api.namecheap.com/xml.response");
@@ -15,6 +15,25 @@ namespace NameCheap
             _params = globalParams;
         }
 
+        /// <summary>
+        /// Sets DNS host records settings for the requested domain.
+        /// </summary>
+        /// <param name="secondLevelDomain">The second level domain, SLD, of the domain for which to set the hosts (the abc in abc.xyz).</param>
+        /// <param name="topLevelDomain">The top-level domain, TLD, of the domain for which to set the hosts (the xyz of abc.xyz).</param>
+        /// <param name="hostEntries">The list of hosts entries to set.
+        /// These need to obey their respective DNS record type rules"
+        /// correct IP address for A-records,
+        /// domain (not IP) for CNAME, etc</param>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found
+        /// - 2016166	Domain is not associated with your account
+        /// - 2030166	Edit permission for domain is not supported
+        /// - 3013288, 4013288	Too many records
+        /// - 3031510	Error From Enom when Errorcount != 0
+        /// - 3050900	Unknown error from Enom
+        /// - 4022288	Unable to get nameserver list
+        /// </exception>
         public void SetHosts(string secondLevelDomain, string topLevelDomain, HostEntry[] hostEntries)
         {
             var query = new Query(_params);
@@ -35,6 +54,22 @@ namespace NameCheap
             XDocument doc = query.Execute("namecheap.domains.dns.setHosts");
         }
 
+        /// <summary>
+        /// Retrieves DNS host record settings for the requested domain..
+        /// </summary>
+        /// <param name="sld">The second level domain, SLD, of the domain for which to get the hosts (the abc in abc.xyz).</param>
+        /// <param name="tld">The top-level domain, TLD, of the domain for which to get the hosts (the xyz of abc.xyz).</param>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found
+        /// - 2030166	Edit permission for domain is not supported
+        /// - 2030288	Cannot complete this command as this domain is not using proper DNS servers
+        /// - 4023330	Unable to get DNS hosts from list
+        /// - 3031510	Error From Enom when Errorcount != 0
+        /// - 3050900	Unknown error from Enom
+        /// - 3011288	Invalid name server specified
+        /// - 5050900	Unhandled Exceptions
+        /// </exception>
         public DnsHostResult GetHosts(string sld, string tld)
         {
             var query = new Query(_params)
@@ -51,6 +86,21 @@ namespace NameCheap
             }
         }
 
+        /// <summary>
+        /// Gets email forwarding settings for the requested domain.
+        /// </summary>
+        /// <param name="domain">the domain for which to get forwarding settings.</param>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found
+        /// - 2030166	Edit permission for domain is not supported
+        /// - 2030288	Cannot complete this command as this domain is not using proper DNS servers
+        /// - 3031510	Error From Enom when Errorcount != 0
+        /// - 3050900	Unknown error from Enom
+        /// - 4022328	Unable to get EmailForwarding records from database
+        /// - 3011288	Invalid nameserver
+        /// - 5050900	Unhandled Exceptions
+        /// </exception>
         public DnsEmailForwardingResult GetEmailForwarding(string domain)
         {
             var query = new Query(_params);
@@ -66,6 +116,22 @@ namespace NameCheap
             }
         }
 
+        /// <summary>
+        /// Sets email forwarding for a domain name.
+        /// </summary>
+        /// <param name="domain">The domain for which to set email forwarding.</param>
+        /// <param name="request">The entire list of forwards to set up.</param>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found 
+        /// - 2016166 Domain is not associated with your account
+        /// - 2030288 Cannot complete this command as this domain is not using proper DNS servers
+        /// - 2030166 Edit Permission for domain is not supported
+        /// - 3013288 Too many records
+        /// - 3031510 Error From Enom when Errorcount != 0
+        /// - 3050900 Unknown error from Enom
+        /// - 4022228 Unable to get nameserver list
+        /// </exception>
         public void SetEmailForwarding(string domain, EmailForwarding[] request)
         {
             var query = new Query(_params)
@@ -84,11 +150,34 @@ namespace NameCheap
         /// Deletes all the email forwarding for a domain.
         /// </summary>
         /// <param name="domain">The domain for which to delete the forwards.</param>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found 
+        /// - 2016166 Domain is not associated with your account
+        /// - 2030288 Cannot complete this command as this domain is not using proper DNS servers
+        /// - 2030166 Edit Permission for domain is not supported
+        /// - 3031510 Error From Enom when Errorcount != 0
+        /// - 3050900 Unknown error from Enom
+        /// - 4022228 Unable to get nameserver list
+        /// </exception>
         public void DeleteAllEmailForwarding(string domain)
         {
             SetEmailForwarding(domain, new EmailForwarding[0]);
         }
 
+        /// <summary>
+        /// Gets a list of DNS servers associated with the requested domain.
+        /// </summary>
+        /// <param name="sld">The second level domain, SLD, of the domain for which to get the list of name servers (the abc in abc.xyz).</param>
+        /// <param name="tld">The top-level domain, TLD, of the domain for which to get the list of name servers (the xyz of abc.xyz).</param>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found
+        /// - 2016166	Domain is not associated with your account
+        /// - 3031510	Error From Enom when Errorcount != 0
+        /// - 3050900	Unknown error from Enom
+        /// - 4022288	Unable to get nameserver list
+        /// </exception>
         public DnsListResult GetList(string sld, string tld)
         {
             var query = new Query(_params)
@@ -105,6 +194,21 @@ namespace NameCheap
             }
         }
 
+        /// <summary>
+        /// Sets domain to use custom DNS servers.
+        /// </summary>
+        /// <param name="sld">The second level domain, SLD, of the domain for which to set the list of name servers (the abc in abc.xyz).</param>
+        /// <param name="tld">The top-level domain, TLD, of the domain for which to set the list of name servers (the xyz of abc.xyz).</param>
+        /// <param name="nameservers">IP address of the custom domain name servers.</param>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found
+        /// - 2016166	Domain is not associated with your account
+        /// - 2030166	Edit permission for domain is not supported
+        /// - 3031510	Error From Enom when Errorcount != 0
+        /// - 3050900	Unknown error from Enom
+        /// - 4022288	Unable to get nameserver list
+        /// </exception>
         public void SetCustom(string sld, string tld, params string[] nameservers)
         {
             var query = new Query(_params)
@@ -115,6 +219,24 @@ namespace NameCheap
             query.Execute("namecheap.domains.dns.setCustom");
         }
 
+        /// <summary>
+        /// Sets domain to use NameCheap's default DNS servers.
+        /// </summary>
+        /// <param name="sld">The second level domain, SLD, of the domain for which to use the default name servers (the abc in abc.xyz).</param>
+        /// <param name="tld">The top-level domain, TLD, of the domain for which to use the default name servers (the xyz of abc.xyz).</param>
+        /// <remarks>
+        /// Required for free services like Host record management, URL forwarding, email forwarding, dynamic dns and other value added services.
+        /// </remarks>
+        /// <exception cref="ApplicationException">
+        /// Exception when the following problems are encountered:
+        /// - 2019166	Domain not found
+        /// - 2016166	Domain is not associated with your account
+        /// - 2030166	Edit permission for domain is not supported
+        /// - 3013288	Too many records
+        /// - 3031510	Error From Enom when Errorcount != 0
+        /// - 3050900	Unknown error from Enom
+        /// - 4022288	Unable to get nameserver list
+        /// </exception>
         public void SetDefault(string sld, string tld)
         {
             var query = new Query(_params)
--- NameCheap/NameCheapApi.cs
+++ NameCheap/NameCheapApi.cs
@@ -4,12 +4,28 @@ using System.Net.Sockets;
 
 namespace NameCheap
 {
+    /// <summary>
+    /// Collection of API methods for programmatically performing operations against NameCheap domains.
+    /// </summary>
     public class NameCheapApi
     {
         private readonly GlobalParameters _params;
         private readonly Lazy<DnsApi> _dnsApi;
         private readonly Lazy<DomainsApi> _domainsApi;
 
+        /// <summary>
+        /// Creates a NameCheap API instance configured with the appropriate credentials.
+        /// All parameters are required.
+        /// </summary>
+        /// <param name="username">The Username on which a command is executed. Generally, the values of ApiUser and UserName parameters are the same.</param>
+        /// <param name="apiUser">Username required to access the API</param>
+        /// <param name="apiKey">Password required used to access the API</param>
+        /// <param name="clientIp">An IP address of the server from which our system receives API calls (only IPv4 can be used).</param>
+        /// <param name="isSandbox">Whether to execute the commands against the sandbox API or the live API.</param>
+        /// <remarks>
+        /// See the '$/docs/running_tests.md' on how to set up a Sandbox account in order
+        /// to test the operations of the API before going live.
+        /// </remarks>
         public NameCheapApi(string username, string apiUser, string apiKey, string clientIp, bool isSandbox = false)
         {
             if (string.IsNullOrWhiteSpace(username))
@@ -59,7 +75,10 @@ namespace NameCheap
             _domainsApi = new Lazy<DomainsApi>(() => new DomainsApi(_params));
         }
 
+        /// <summary>Gets the set of functions responsible for domain management (creating, info, renewal, contacts, etc).</summary>
         public DomainsApi Domains => _domainsApi.Value;
+
+        /// <summary>Gets the set of functions used to manipulate DNS records (hosts, name servers, and even email forwarding).</summary>
         public DnsApi Dns => _dnsApi.Value;
     }
 }
